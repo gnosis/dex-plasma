5a3eab00387ea4571f05ff01784d12aa1921ec9c
Ben Smith <admin@admins-mbp.intern>
2018-09-03 15:05:27 +0200

    copy travis files from dx-contracts

---
+++ travscripts/BranchScript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $TRAVIS_BRANCH = "master" || $TRAVIS_BRANCH = "develop" ]]; then
+  echo " ==> Detected PRINCIPAL branch - compiling and testing contracts"
+  #jump back to root
+  cd $TRAVIS_BUILD_DIR
+  echo " ==> JUMPING LOCATIONS: NOW IN $TRAVIS_BUILD_DIR"
+
+  # running contracts tests
+  echo " ==> RUNNING test"
+  npm test;
+else
+  echo " ==> No execution for branches other than MASTER or DEVELOP"
+fi;
