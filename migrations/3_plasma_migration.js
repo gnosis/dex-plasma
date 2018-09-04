65ef546c21bbacddcc67e6be3456fc4e79ea17bc
Ben Smith <admin@admins-MBP.local>
2018-09-04 11:52:41 +0200

    Deploy Priority Queue in plasma_migration script

--- migrations/3_plasma_migration.js
+++ migrations/3_plasma_migration.js
@@ -1,8 +1,9 @@
 
 const EtherToken = artifacts.require("EtherToken")
 const Plasma = artifacts.require("Plasma.sol")
-
+const PriorityQueue = artifacts.require("PriorityQueue.sol");
 
 module.exports = function(deployer, networks, accounts) {
+    deployer.deploy(PriorityQueue)
     deployer.deploy(Plasma, accounts[0], EtherToken.address)
 }
