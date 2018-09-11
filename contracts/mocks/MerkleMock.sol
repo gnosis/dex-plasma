adccac6f8f08664d1c8aa2d148c02d63014df7aa
Ben Smith <admin@admins-mbp.intern>
2018-09-11 12:30:13 +0200

    Updade Merkle and Mock to remove confusion and duplicate function

--- contracts/mocks/MerkleMock.sol
+++ contracts/mocks/MerkleMock.sol
@@ -4,15 +4,11 @@ import "../utils/Merkle.sol";
 
 
 contract MerkleMock {
-
     function checkMembership(bytes32 leaf, uint256 index, bytes32 rootHash, bytes proof, uint height) 
         public
         pure
         returns (bool)
     {   
-        if (height == 16) {
-            return Merkle.checkMembership(leaf, index, rootHash, proof);
-        }
         return Merkle.checkMembership(leaf, index, rootHash, proof, height);
     }
 }
--- contracts/utils/Merkle.sol
+++ contracts/utils/Merkle.sol
@@ -6,29 +6,7 @@ pragma solidity ^0.4.24;
  * @dev Checks that a particular leaf node is in a given Merkle tree given the index, root hash, and a proof
  */
 library Merkle {
-    function checkMembership(bytes32 leaf, uint256 index, bytes32 rootHash, bytes proof)
-        internal
-        pure
-        returns (bool)
-    {
-        require(proof.length == 512);
-        bytes32 proofElement;
-        bytes32 computedHash = leaf;
-
-        for (uint256 i = 32; i <= 512; i += 32) {
-            assembly {
-                proofElement := mload(add(proof, i))
-            }
-            if (index % 2 == 0) {
-                computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
-            } else {
-                computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
-            }
-            index = index / 2;
-        }
-        return computedHash == rootHash;
-    }
-
+    
     function checkMembership(bytes32 leaf, uint256 index, bytes32 rootHash, bytes proof, uint height)
         internal
         pure
