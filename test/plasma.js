1f3e5bacab7b8d61a5b04cd06b4415c132349d19
Ben Smith <admin@admins-mbp.intern>
2018-09-14 15:45:50 +0200

    reject deposit exit (bad depositPosition)

--- test/plasma.js
+++ test/plasma.js
@@ -1,4 +1,4 @@
-let ether = 10**18
+let oneETH = 10**18
 
 
 const EtherToken = artifacts.require("EtherToken")
@@ -6,6 +6,11 @@ const Plasma = artifacts.require("Plasma.sol")
 
 var etherToken
 var plasma
+
+let {
+  assertRejects,
+  toHex
+} = require('./utilities.js');
 
 contract('Plasma', (accounts) => {
   const [operator, depositor] = accounts
@@ -17,28 +22,32 @@ contract('Plasma', (accounts) => {
 
 
   describe('Deposit Tests', () => {
-    before(async () => {
-    })
-
-    after(async () => {
-    })
 
     it('step 1 - Wrap Ether', async () => {
       // ASSERT Auction has started
-      
-      await etherToken.deposit({from: depositor, value: ether});
-      await etherToken.approve(Plasma.address, ether, {from: depositor})
-     
+      await etherToken.deposit({from: depositor, value: oneETH})
+      await etherToken.approve(Plasma.address, oneETH, {from: depositor})
     })
 
-    it('step 2 - deposits', async () => { 
-      var currentDepositBlock = (await plasma.currentDepositBlock.call()).toNumber()
+    it('step 2 - deposits', async () => {
+      const currentDepositBlock = (await plasma.currentDepositBlock.call()).toNumber()
 
-      await plasma.deposit(ether, 0, {from: depositor})
+      await plasma.deposit(oneETH, 0, {from: depositor})
 
       var currentDepositBlockNew = (await plasma.currentDepositBlock.call()).toNumber()
-
       assert.equal(currentDepositBlock+1, currentDepositBlockNew, "new deposit has not been correctly credited")
+    })
+
+    it('Rejected exitDeposit', async () => {
+
+      await assertRejects(plasma.startDepositExit(1000000000, 0, oneETH, {from: depositor}))
+
+    })
+
+    it('Rejected exitDeposit', async () => {
+
+      await assertRejects(plasma.startDepositExit(1000000000, 0, oneETH, {from: depositor}))
+
     })
 
   })
